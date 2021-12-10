303a488273072821ef4b4d79a7520c347f7292f7
unbxd <unbxd@192.168.100.26>
2021-12-10 22:52:25 +0530

    basic2

--- studentscrud5/src/main/java/com/studentscrud/repository/StudentRepository.java
+++ studentscrud5/src/main/java/com/studentscrud/repository/StudentRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.studentscrud.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-  //List<Student> findByPublished(boolean published);
-  //List<Student> findById(String title);
+ 
 }
